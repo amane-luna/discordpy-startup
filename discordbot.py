671bf7b0e14c2f1a7d6f567a0ddc731e5c15cc14
amane-luna <77225484+amane-luna@users.noreply.github.com>
2021-01-11 14:06:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,6 +26,8 @@ async def on_message(message):
         await message.channel.send('私もですよ...//')
     if message.content == 'いい子だ':
         await message.channel.send('ますたーに褒められちゃった...//')
+    if message.content == 'あまね！':
+        await message.channel.send('あっ///ご主人様///')
     if message.content == 'cleanup':
         if message.author.guild_permissions.administrator:
             await message.channel.purge()
